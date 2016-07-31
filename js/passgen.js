4c7d9098bbd56f3817882c2231f0d26e2766ae09
DarkSnake-Kobra <jbttoms@gmail.com>
2016-07-31 06:28:34 -0500

    added output box for password and few other things with lots of repo cleanup
    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- js/passgen.js
+++ js/passgen.js
@@ -3,16 +3,22 @@ function passGen()
 	var characters = "01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	var charLength = characters.length;
 	
-	var size = 20;
+	var size;
 	var count = 0;
 	var temp;
-	var password = ""
+	var password = "";
 	
+	size = document.getElementById('size').value;
 	while(count < size)
 	{
 		temp = Math.floor((Math.random() * charLength));
 		password += characters.charAt(temp);
 		count++;
 	}
-	alert(password);
+	document.getElementById('password').value= password;
+}
+
+function reset()
+{
+	document.getElementById('password').value= "";
 }
