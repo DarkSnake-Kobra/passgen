501de9889ef21fdd4a64b2c47867d1f9ae56b90e
DarkSnake-Kobra <jbttoms@gmail.com>
2017-01-25 03:45:41 -0600

    added special character support
    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- app.js
+++ app.js
@@ -1,42 +1,117 @@
-function passGen()
+function PASSGEN()
 {
+    var numbers_state;
+	var symbols_state;
 
+	numbers_state = document.getElementById('numbers').checked;
+    symbols_state = document.getElementById('symbols').checked;
+
+	if (numbers_state == true)
+	{
+		NUMBERS();
+	}
+    if (numbers_state == false)
+    {
+        ALPHABET();
+    }
+    if (symbols_state == true && numbers_state == true)
+    {
+        GENERATE_EVERYTHING();
+    }
+    if (symbols_state == true && numbers_state == false)
+    {
+        SYMBOLS();
+    }
+}
+
+function NUMBERS()
+{
+		
 	// store letters and get length
-	var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-	// store numbers and get length
-	var numbers = "0123456789"
+	var characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	var size;
 	var count = 0;
 	var temp;
 	var password = "";
-	var numbersState;
-	var holdChar = "";
-	var holdCharLength;
+	var characters_length = characters.length;
 
 	size = document.getElementById('size').value;
-	numbersState = document.getElementById('numbers').checked;
-
-	if (numbersState == true)
-	{
-		holdChar = numbers + letters;
-		holdCharLength = holdChar.length;
-	}
-	else
-	{
-		holdChar = letters;
-		holdCharLength = holdChar.length
-	}
 
 	while(count < size)
 	{
-	  temp = Math.floor((Math.random() * holdCharLength));
-	  password += holdChar.charAt(temp);
+	  temp = Math.floor((Math.random() * characters_length));
+	  password += characters.charAt(temp);
 	  count++;
 	}
 	document.getElementById('password').value= password;
+}
+
+function ALPHABET()
+{
+    // store letters and get length
+	var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	var size;
+	var count = 0;
+	var temp;
+	var password = "";
+	var characters_length = characters.length;
+
+	size = document.getElementById('size').value;
+
+	while(count < size)
+	{
+	  temp = Math.floor((Math.random() * characters_length));
+	  password += characters.charAt(temp);
+	  count++;
 	}
+	document.getElementById('password').value= password;
+}
+
+function GENERATE_EVERYTHING()
+{
+    // store letters and get length
+	var characters = "!@#$%^&*()_-+=<>,.?{}[];:0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY";
+
+	var size;
+	var count = 0;
+	var temp;
+	var password = "";
+	var characters_length = characters.length;
+
+	size = document.getElementById('size').value;
+
+	while(count < size)
+	{
+	  temp = Math.floor((Math.random() * characters_length));
+	  password += characters.charAt(temp);
+	  count++;
+	}
+	document.getElementById('password').value= password;
+}
+
+function SYMBOLS()
+{
+     // store letters and get length
+	var characters = "!@#$%^&*()_-+=<>,.?{}[];:abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY";
+
+	var size;
+	var count = 0;
+	var temp;
+	var password = "";
+	var characters_length = characters.length;
+
+	size = document.getElementById('size').value;
+
+	while(count < size)
+	{
+	  temp = Math.floor((Math.random() * characters_length));
+	  password += characters.charAt(temp);
+	  count++;
+	}
+	document.getElementById('password').value= password;
+}
 
 function reset()
 {
