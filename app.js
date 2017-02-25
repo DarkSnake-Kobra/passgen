5f0f61ed3509315ea7c421533583388b45561b56
DarkSnake-Kobra <jbttoms@gmail.com>
2017-02-25 02:40:22 -0600

    added option to save password size, minor ui text tweaks and repo cleanup
    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var saveToLog;
+var configPath =__dirname + "\\" + "data" + "\\" + "settings.conf";
 
 function passgen()
 {
@@ -133,19 +134,38 @@ function about()
 {
 	alert("Copyright (c) 2017 Jordan Bottoms\n" +
 	"Released under the Apache 2 License\n" +
-	"Website: jordanbottoms.com");
+	"Website: jordanbottoms.com", "About");
 }
 
+function readConfig()
+{
+    var config = fs.readFileSync(configPath, 'utf8');
+    document.getElementById('size').value= config;
+}
 
 function createLog()
 {
     saveToLog = saveToLog + '\n'
-    log = __dirname +"\\" + "data" + "\\" + "log"
+    log = __dirname +"\\" + "data" + "\\" + "backup.log"
     fs.appendFile(log, saveToLog,(err) => {
     if (err) throw err;
     console.log('The "data to append" was appended to file!');
 });
 }
+
+function saveSettings()
+{
+	var size = document.getElementById('size').value;
+	fs.writeFile(configPath, size, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+    alert("Settings saved", "Alert");
+}); 
+}
+
 function closeWin()
 {
 	window.close();
