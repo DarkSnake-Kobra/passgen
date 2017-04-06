b25df2ce68f00fcc3be9217296d806d20d07e260
DarkSnake-Kobra <jbttoms@gmail.com>
2017-04-06 09:48:24 -0500

    added a few comments and few minor adjustments
    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/* Copyright 2017 Jordan Bottoms
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
 /* passgen
  * 
  * This contains the password generator and quit functions only. 
@@ -5,25 +21,34 @@
  
 function passgen()
 {
-    var numbers_state;
-	var symbols_state;
+	checkedState()
+}
 
-	numbers_state = document.getElementById('numbers').checked;
-    symbols_state = document.getElementById('symbols').checked;
+function checkedState()
+{
+	
+	/* Determine checked boxes checked state
+	 * then call appropriate functions
+	 */
+	var numbersState;
+	var symbolsState;
 
-	if (numbers_state == true)
+	numbersState = document.getElementById('numbers').checked;
+    symbolsState = document.getElementById('symbols').checked;
+
+	if (numbersState == true)
 	{
 		numbers();
 	}
-    if (numbers_state == false)
+    if (numbersState == false)
     {
         alphabet();
     }
-    if (symbols_state == true && numbers_state == true)
+    if (symbolsState == true && numbersState == true)
     {
         createAll();
     }
-    if (symbols_state == true && numbers_state == false)
+    if (symbolsState == true && numbersState == false)
     {
         symbols();
     }
@@ -31,24 +56,36 @@ function passgen()
 
 function numbers()
 {
-		
-	// store letters and get length
+	// Set characters to lower/upper case
+	// and numbers
 	var characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 	var size;
 	var count = 0;
 	var temp;
 	var password = "";
 	var charLength = characters.length;
 
+	// get size text boxes value
+	// to determine password size
 	size = document.getElementById('size').value;
 
+	/* Loop through the characters variable
+	 * by using the length as the end and
+	 * store the retrieved value in temp then
+	 * increment it by 1 and stopping if greater
+	 * then size variable
+	 */
 	while(count < size)
 	{
 	  temp = Math.floor((Math.random() * charLength));
 	  password += characters.charAt(temp);
 	  count++;
 	}
+	
+	/* Set password box to password variable
+	 * then call the log funtion that stores
+	 * the password in the log file for backup
+	 */
 	document.getElementById('password').value= password;
     createLog();
 }
@@ -57,7 +94,6 @@ function alphabet()
 {
     // store letters and get length
 	var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 	var size;
 	var count = 0;
 	var temp;
