7d41e90d8a7cf3874ebcd62de8152961b57fa220
DarkSnake-Kobra <jbttoms@gmail.com>
2017-04-28 01:49:04 -0500

    added a simple menu, copy to clipboard function/button and changed to mit license.
    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- app/passgen.js
+++ app/passgen.js
@@ -1,25 +1,20 @@
-/* Copyright 2017 Jordan Bottoms
+/*
+Copyright 2017 Jordan Bottoms
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-       http://www.apache.org/licenses/LICENSE-2.0
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 
 /* passgen
- * 
+ *
  * Core of the program. Creates password and returns it to the window to display it*/
- 
+
 function passgen(){
-	
+
 	/* Determine checked boxes checked state
 	 * then call appropriate functions
 	 */
@@ -96,7 +91,7 @@ function random(characters){
 	  password += characters.charAt(temp);
 	  count++;
 	}
-	
+
 	/* Set password box to password variable
 	 * then call the log funtion that stores
 	 * the password in the log file for backup
